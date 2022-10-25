112c4dbbca19b6e02f368f1bd5cd7912982829a6
Nick Ruffles <nick.ruffles@engineerbetter.com>
2022-10-25 11:00:12 +0100

    chore: removed unused code

--- poker/CLI.go
+++ poker/CLI.go
@@ -4,35 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"os"
+	"log"
 	"strconv"
 	"strings"
-	"time"
 )
 
 func (cli *CLI) getPlayerCount() int {
-	fmt.Fprint(cli.out, PlayerPrompt)
+	_, err := fmt.Fprint(cli.out, PlayerPrompt)
+	if err != nil {
+		return 0
+	}
 	noOfPlayers, _ := strconv.Atoi(cli.readLine())
 	//if err != nil {
 	//	fmt.Fprint(cli.out, ErrorPlayerPrompt)
 	//	return cli.getPlayerCount()
 	//}
 	return noOfPlayers
-}
-
-func (cli *CLI) quit() {
-	fmt.Fprint(cli.out, "Thanks for playing!")
-	os.Exit(0)
-}
-
-func (cli *CLI) scheduleBlindAlerts(noOfPlayers int) {
-	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
-	blindTime := 0 * time.Second
-	for _, blind := range blinds {
-		fmt.Println(blind)
-		//cli.alerter.ScheduleAlertAt(blindTime, blind)
-		blindTime += 10 * time.Minute
-	}
 }
 
 type CLI struct {
@@ -51,19 +38,23 @@ func NewCLI(in io.Reader, out io.Writer, game Game) *CLI {
 
 const (
 	PlayerPrompt         = "Please enter the number of players: "
-	ErrorPlayerPrompt    = "You did not enter a valid integer, try again"
-	WinnerPrompt         = "Please enter the winner: "
 	BadPlayerInputErrMsg = "Incorrect input, please try again"
 )
 
 func (cli *CLI) PlayPoker() {
-	fmt.Fprint(cli.out, PlayerPrompt)
+	_, err := fmt.Fprint(cli.out, PlayerPrompt)
+	if err != nil {
+		log.Fatalf("failed to log to client output")
+	}
 
 	numberOfPlayersInput := cli.readLine()
 	numberOfPlayers, err := strconv.Atoi(strings.Trim(numberOfPlayersInput, "\n"))
 
 	if err != nil {
-		fmt.Fprint(cli.out, BadPlayerInputErrMsg)
+		_, err := fmt.Fprint(cli.out, BadPlayerInputErrMsg)
+		if err != nil {
+			log.Fatal("failed to log to client out")
+		}
 		return
 	}
 
